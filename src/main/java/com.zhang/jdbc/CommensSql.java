1659cc0c615e1333b2c984b500aedaf83a9b6c25
Administrator <1746678613@qq.com>
2020-12-02 09:39:52 +0800

    bran1_Jdbc通用方法curd可变参数

---
+++ src/main/java/com.zhang/jdbc/CommensSql.java
@@ -0,0 +1,42 @@
+package com.zhang.jdbc;
+
+import com.zhang.utils.JdbcUtils;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.jdbc
+ * @date:2020/12/2
+ */
+public class CommensSql {
+
+    public static void commensSql(String sql,Object ...args){
+        Connection con=null;
+        PreparedStatement ps=null;
+        try {
+            con = JdbcUtils.getConnection();
+            ps = con.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+
+              ps.setObject(i+1,args[i]);
+            }
+            ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(ps,con);
+        }
+    }
+
+    @Test
+    public void testDelete(){
+        String sql="delete from customers where id=?";
+        commensSql(sql,20);
+
+    }
+}
