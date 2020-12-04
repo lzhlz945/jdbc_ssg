0b3f1e9d7752c87a128324f2d95c8f3807f88685
Administrator <1746678613@qq.com>
2020-12-04 11:14:17 +0800

    bran1_jdbc_druid连接池初始化

---
+++ src/main/java/com.zhang/druid/DruidTest.java
@@ -0,0 +1,41 @@
+package com.zhang.druid;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.druid
+ * @date:2020/12/4
+ */
+public class DruidTest {
+
+    private static DataSource source;
+    static {
+        try {
+            FileInputStream fis=new FileInputStream("");
+            Properties properties=new Properties();
+            properties.load(fis);
+            source= DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public Connection test01() throws SQLException {
+        Connection connection = source.getConnection();
+        return connection;
+
+    }
+
+
+}
